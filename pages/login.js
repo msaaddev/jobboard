c6b4aaccca621908f531eee7d3aa7d6068cf56a0
msaaddev <mrsaadirfan@gmail.com>
2021-07-14 02:08:59 +0500

    📦 NEW: login page

---
+++ pages/login.js
@@ -0,0 +1,145 @@
+import { useEffect, useContext } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { AuthContext } from '../components/context/AuthContext';
+import fire from '../utils/firebase';
+import styles from '../styles/auth.module.css';
+
+const Login = () => {
+	const [
+		user,
+		setUser,
+		email,
+		setEmail,
+		password,
+		setPassword,
+		emailErr,
+		setEmailErr,
+		passwordErr,
+		setPasswordErr
+	] = useContext(AuthContext);
+	const router = useRouter();
+
+	/**
+	 *
+	 *
+	 * reset values ofo inputs to empty string
+	 */
+	const clearInput = () => {
+		setEmail('');
+		setPassword('');
+	};
+
+	/**
+	 *
+	 *
+	 * reset values of errors to empty string
+	 */
+	const clearErrs = () => {
+		setEmailErr('');
+		setPasswordErr('');
+	};
+
+	/**
+	 *
+	 *
+	 * logs in the systems
+	 */
+	const handleLogin = async () => {
+		clearErrs();
+
+		fire.auth()
+			.signInWithEmailAndPassword(email, password)
+			.then(() => {
+				router.push('/');
+			})
+			.catch((err) => {
+				const { code, message } = err;
+
+				if (
+					code === 'auth/invalid-email' ||
+					code === 'auth/user-disabled' ||
+					code === 'auth/user-not-found'
+				) {
+					setEmailErr(message);
+				}
+
+				if (code === 'auth/wrong-password') {
+					setPasswordErr(message);
+				}
+			});
+	};
+
+	/**
+	 *
+	 *
+	 * checks where the user is logged in or not
+	 */
+	const authListener = () => {
+		fire.auth().onAuthStateChanged((user) => {
+			if (user) {
+				clearInput();
+				setUser(user);
+			} else {
+				setUser('');
+			}
+		});
+	};
+
+	useEffect(() => {
+		authListener();
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.subcontainer}>
+				<div className={styles.email}>
+					<label htmlFor="email" className={styles.label}>
+						Email
+					</label>
+					<input
+						type="email"
+						autoFocus
+						required
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						className={styles.input}
+					/>
+					{emailErr !== '' && (
+						<p className={styles.err}>{emailErr}</p>
+					)}
+				</div>
+				<div className={styles.password}>
+					<label htmlFor="password" className={styles.label}>
+						Password
+					</label>
+					<input
+						type="password"
+						required
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						className={styles.input}
+					/>
+					{passwordErr !== '' && (
+						<p className={styles.err}>{passwordErr}</p>
+					)}
+				</div>
+				<div className={styles.signup}>
+					<button onClick={handleLogin}>Login</button>
+				</div>
+				<div className={styles.msg}>
+					<p>
+						Don't have an account?{' '}
+						<span>
+							<Link href="/signup">
+								<a>Sign Up</a>
+							</Link>
+						</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
