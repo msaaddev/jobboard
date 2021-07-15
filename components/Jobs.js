bdaaf5f69d7d70b254365add245c6797164be84b
msaaddev <mrsaadirfan@gmail.com>
2021-07-15 16:16:30 +0500

    👌 IMPROVE: code refactoring

--- components/Jobs.js
+++ components/Jobs.js
@@ -1,5 +1,5 @@
+import Job from './common/Job';
 import styles from '../styles/jobs.module.css';
-import Image from 'next/image';
 
 const Jobs = ({ day, jobs }) => {
 	return (
@@ -10,29 +10,14 @@ const Jobs = ({ day, jobs }) => {
 			<div className={styles.jobs}>
 				{jobs.map((job) => {
 					return (
-						<div
-							className={styles.job_container}
-							key={Math.random() * 10000}
-						>
-							<p className={styles.initial}>{job.letter}</p>
-							<div className={styles.job_info}>
-								<div className={styles.title_wrapper}>
-									<p className={styles.title}>{job.title}</p>
-									<p className={styles.date}>{job.date}</p>
-								</div>
-								<p className={styles.company}>{job.company}</p>
-								<p className={styles.location}>
-									{' '}
-									<Image
-										src="/location.png"
-										alt="location"
-										width={14}
-										height={14}
-									/>{' '}
-									{job.location}
-								</p>
-							</div>
-						</div>
+						<Job
+							key={job.id}
+							letter={job.letter}
+							title={job.title}
+							date={job.date}
+							company={job.company}
+							location={job.location}
+						/>
 					);
 				})}
 			</div>
---
+++ components/common/Job.js
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import styles from '../../styles/job.module.css';
+
+const Job = ({ letter, title, date, company, location }) => {
+	return (
+		<div className={styles.container}>
+			<p className={styles.initial}>{letter}</p>
+			<div className={styles.job_info}>
+				<div className={styles.title_wrapper}>
+					<p className={styles.title}>{title}</p>
+					<p className={styles.date}>{date}</p>
+				</div>
+				<p className={styles.company}>{company}</p>
+				<p className={styles.location}>
+					{' '}
+					<Image
+						src="/location.png"
+						alt="location"
+						width={14}
+						height={14}
+					/>{' '}
+					{location}
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Job;
