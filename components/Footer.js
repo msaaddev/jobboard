052be9df9149f85258e8c7a6c73045052ddabd36
msaaddev <mrsaadirfan@gmail.com>
2021-07-16 02:30:18 +0500

    📦 NEW: footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import styles from '../styles/footer.module.css';
+
+const Footer = () => {
+	return (
+		<div className={styles.container}>
+			<h2>Footer</h2>
+		</div>
+	);
+};
+
+export default Footer;
