d515cde5741ae413dd2f80c30b8cc966162e1690
msaaddev <mrsaadirfan@gmail.com>
2021-07-15 20:34:59 +0500

    📦 NEW: options

--- components/common/Input.js
+++ components/common/Input.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styles from '../../styles/input.module.css';
 
 const Input = ({
@@ -7,12 +8,34 @@ const Input = ({
 	autoFocus = false,
 	value,
 	handleOnChange,
-	err,
-	dropdown = false
+	err = '',
+	dropdown = false,
+	widthMax = false,
+	firstChildtopMargin = false,
+	mediumMargin = false
 }) => {
+	const [container, setContainer] = useState(styles.container);
+
+	useEffect(() => {
+		let temp;
+
+		if (widthMax) {
+			temp = container;
+			setContainer(`${temp} ${styles.width}`);
+
+			if (firstChildtopMargin) {
+				setContainer(`${temp} ${styles.width} ${styles.top_margin}`);
+			}
+
+			if (mediumMargin) {
+				setContainer(`${temp} ${styles.width} ${styles.medium_margin}`);
+			}
+		}
+	}, []);
+
 	return (
 		<>
-			<div className={styles.container}>
+			<div className={container}>
 				{!dropdown ? (
 					<>
 						<label htmlFor={htmlFor} className={styles.label}>
@@ -27,6 +50,20 @@ const Input = ({
 							className={styles.input}
 						/>
 						{err !== '' && <p className={styles.err}>{err}</p>}
+					</>
+				) : label === 'Type' ? (
+					<>
+						<label htmlFor={htmlFor} className={styles.label}>
+							{label}
+						</label>
+						<select
+							onChange={(e) => handleOnChange(e.target.value)}
+						>
+							<option value="full_time">Full time</option>
+							<option value="part_time">Part time</option>
+							<option value="contract">Contract</option>
+							<option value="internship">Internship</option>
+						</select>
 					</>
 				) : (
 					<>
