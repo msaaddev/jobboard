20d93d1e56f8425beff95727a1c21f6de0bfa033
msaaddev <mrsaadirfan@gmail.com>
2021-07-23 15:32:07 +0500

    👌 IMPROVE: footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,10 @@ import styles from '../styles/footer.module.css';
 const Footer = () => {
 	return (
 		<div className={styles.container}>
-			<h2>Footer</h2>
+			<h3>
+				Made by <a href="https://github.com/msaaddev">Saad Irfan</a>{' '}
+				with 💜
+			</h3>
 		</div>
 	);
 };
