8ec1cc71fedec448d85d82165f2ce14241f24769
msaaddev <mrsaadirfan@gmail.com>
2021-07-23 18:32:41 +0500

    👌 IMPROVE: code

--- components/context/JobContext.js
+++ components/context/JobContext.js
@@ -1,6 +1,4 @@
 import { useState, createContext } from 'react';
-import JobData from '../../data/jobs';
-
 const JobContext = createContext();
 
 const JobProvider = ({ children }) => {
--- data/jobs.js
+++
@@ -1,9 +0,0 @@
-const jobData = [];
-
-/**
-*
-* @param {jobs} - array of all jobs
-*/
-const jobs = (param) => {}
-
-export default jobData;
